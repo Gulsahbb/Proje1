d264571a09786c9401d8403b6191998d0920422c
Gülşah Başkurt <Gulsah.baskurt@ogr.dpu.edu.tr>
2021-03-29 14:08:10 +0300

    Add project files.

---
+++ Proje1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Proje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.sayıyı giriniz : ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("2.sayıyı giriniz : ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("3.sayıyı giriniz : ");
+            int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 > sayi2 && sayi1 > sayi3)
+            {
+                Console.WriteLine("En büyük değer : " + sayi1);
+            }
+            else if(sayi2 > sayi1 && sayi2 > sayi3){
+                Console.WriteLine("En büyük değer : " + sayi2 );
+            }
+            else
+            {
+                Console.WriteLine("En büyük değer : " + sayi3);
+            }
+
+            if (sayi1 < sayi2 && sayi1 < sayi3)
+            {
+                Console.WriteLine("En küçük değer : " + sayi1);
+            }
+            else if (sayi2 < sayi1 && sayi2 < sayi3)
+            {
+                Console.WriteLine("En küçük değer : " + sayi2);
+            }
+            else
+            {
+                Console.WriteLine("En küçük değer : " + sayi3);
+            }
+
+        }
+    }
+}
